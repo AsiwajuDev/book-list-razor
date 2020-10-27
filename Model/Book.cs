06ae222315a3ac6f18f42c8e70418dcb771736cd
asiwajudev <olanrewajuranda@gmail.com>
2020-10-28 00:41:35 +0100

    Connection Strings config added, DBContext class added and first migration to Database completed

---
+++ Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        //key maps to Id
+        [Key]
+        public int Id { get; set; }
+
+        //Required maps to Name to make it a required field
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -10,16 +10,16 @@ namespace BookListRazor.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IActionResult OnGet()
         {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
+            return Page();
         }
     }
 }
