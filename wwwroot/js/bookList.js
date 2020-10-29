61e22925ae9e0d0c2c3c039118cc3eff9e0c289d
asiwajudev <olanrewajuranda@gmail.com>
2020-10-29 03:29:13 +0100

    Delete API implemented and consumed in the JS Datatable with Toastr notifications

--- wwwroot/js/bookList.js
+++ wwwroot/js/bookList.js
@@ -22,7 +22,7 @@ function loadDataTable() {
                     <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width: 100px;">
                         Edit
                     </a>
-                    <a class="btn btn-danger text-white" style="cursor:pointer; width: 100px;">
+                    <a onclick=Delete('/api/book/?id='+${data}) class="btn btn-danger text-white" style="cursor:pointer; width: 100px;">
                         Delete
                     </a>
                     </div>`;
@@ -34,5 +34,31 @@ function loadDataTable() {
             "emptyTable": "no data found"
         },
         "width": "100%"
+    })
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message)
+                    }
+                }
+            })
+        }
     })
 }
